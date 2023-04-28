175f4f97f3a73bbf50877af9df73e1fabc786afd
Nick_Tomp <ws1011122@gmail.com>
2023-04-28 12:53:42 +0400

    fix: завершить рефакторинг кода

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,18 @@
 //Объявление переменных (для profile)
-const ProfilePopup = document.querySelector('#profile-popup');
-const ProfileFormElement = ProfilePopup.querySelector('.popup__form');
-const nameInput = ProfileFormElement.querySelector('#name-input');
-const jobInput = ProfileFormElement.querySelector('#job-input');
+const profilePopup = document.querySelector('#profile-popup');
+const profileFormElement = profilePopup.querySelector('.popup__form');
+const nameInput = profileFormElement.querySelector('#name-input');
+const jobInput = profileFormElement.querySelector('#job-input');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 const editProfileButton = document.querySelector('.profile__button');
-const ProfileCloseButton = ProfilePopup.querySelector('.popup__close-button');
 //Объявление переменных (для image)
-const ImagePopup = document.querySelector('#image-popup');
-const ImageFormElement = ImagePopup.querySelector('.popup__form');
-const titleInput = ImageFormElement.querySelector('#title-input');
-const linkInput = ImageFormElement.querySelector('#link-input');
+const imagePopup = document.querySelector('#image-popup');
+const imageForm = imagePopup.querySelector('#add-image-form');
+const imageFormElement = imagePopup.querySelector('.popup__form');
+const titleInput = imageFormElement.querySelector('#title-input');
+const linkInput = imageFormElement.querySelector('#link-input');
 const addButton = document.querySelector('.profile__add-button');
-const ImageCloseButton = ImagePopup.querySelector('.popup__close-button');
 const initialCards = [
     {
         name: 'Архыз',
@@ -47,28 +46,27 @@ const initialCards = [
     }
 ];
 const imageTemplate = document.querySelector('#image-element').content;
-let cardsList = document.querySelector('.elements');
+const cardsList = document.querySelector('.elements');
 const viewPopup = document.querySelector('#view-popup');
 const viewPopupImage = viewPopup.querySelector('.popup__image');
 const viewPopupText = viewPopup.querySelector('.popup__caption');
 const viewPopupCloseButton = viewPopup.querySelector('.popup__close-button');
-//Создание первичных событий, объявление вторичных переменных
+const closeButtons = document.querySelectorAll('.popup__close-button');
+//Создание первичных событий
 addCardsArray();
-const cards = document.querySelector('.elements');
-const deleteButton = cards.querySelectorAll('.elements__delete-button');
-const likeButton = cards.querySelectorAll('.elements__like-button');
-const imageContent = cards.querySelectorAll('.elements__image');
 //Объявление функций 
+function openPopup(popup) {
+    popup.classList.add('popup_opened');
+}
+function closePopup(popup) {
+    popup.classList.remove('popup_opened');
+}
 //(для profile)
-function openProfilePopup() {
-    ProfilePopup.classList.add('popup_opened');
+function startProfilePopup() {
     const userName = profileName.textContent;
     const userJob = profileJob.textContent;
     nameInput.value = userName;
     jobInput.value = userJob;
-}
-function closeProfilePopup() {
-    ProfilePopup.classList.remove('popup_opened');
 }
 function handleProfileFormSubmit(evt) {
     evt.preventDefault();
@@ -76,66 +74,44 @@ function handleProfileFormSubmit(evt) {
     const userJob = jobInput.value;
     profileName.textContent = userName;
     profileJob.textContent = userJob;
-    closeProfilePopup();
+    closePopup(profilePopup);
 }
 //(для image)
+function createCard() {
+    imageElement = imageTemplate.querySelector('.elements__element').cloneNode(true);
+    const deleteButton = imageElement.querySelector('.elements__delete-button');
+    const likeButton = imageElement.querySelector('.elements__like-button');
+    const imageContent = imageElement.querySelector('.elements__image');
+    deleteButton.addEventListener('click', function (evt) {
+        deleteCard(evt);
+    })
+    likeButton.addEventListener('click', function (evt) {
+        likeToggle(evt);
+    })
+    imageContent.addEventListener('click', function (evt) {
+        openImageViewPopup(evt);
+    })
+    return imageElement
+}
 function addCardsArray() {
     for (let i = 0; i < initialCards.length; i++) {
-        const imageElement = imageTemplate.querySelector('.elements__element').cloneNode(true);
+        const imageElement = createCard();
         imageElement.querySelector('.elements__image').src = initialCards[i].link;
         imageElement.querySelector('.elements__image').alt = initialCards[i].alt;
         imageElement.querySelector('.elements__text').textContent = `${initialCards[i].name}`;
         cardsList.append(imageElement);
     }
 }
-function initializeArray(deleteButton, likeButton, imageContent) {
-    //Назначения для первичных кнопок delete
-    for (let i = 0; i < deleteButton.length; i++) {
-        deleteButton[i].addEventListener('click', function (evt) {
-            deleteCard(evt);
-        })
-    };
-    //Назначения для первичных кнопок like
-    for (let i = 0; i < likeButton.length; i++) {
-        likeButton[i].addEventListener('click', function (evt) {
-            likeToggle(evt);
-        })
-    };
-    //Назначения для первичных кнопок view
-    for (let i = 0; i < imageContent.length; i++) {
-        imageContent[i].addEventListener('click', function (evt) {
-            openImageViewPopup(evt);
-        })
-    };
-};
-function openImageAddPopup() {
-    ImagePopup.classList.add('popup_opened');
-    titleInput.value = '';
-    linkInput.value = '';
-}
-function closeImageAddPopup() {
-    ImagePopup.classList.remove('popup_opened');
-}
 function handleImageFormSubmit(evt) {
     evt.preventDefault();
     const imageTitle = titleInput.value;
     const imageLink = linkInput.value;
-    const imageElement = imageTemplate.querySelector('.elements__element').cloneNode(true);
+    const imageElement = createCard();
     imageElement.querySelector('.elements__image').src = imageLink;
     imageElement.querySelector('.elements__image').alt = imageTitle;
     imageElement.querySelector('.elements__text').textContent = `${imageTitle}`;
     cardsList.prepend(imageElement);
-    const newDeleteButton = imageElement.querySelector('.elements__delete-button');
-    newDeleteButton.addEventListener('click', function (evt) {
-        deleteCard(evt);
-    });
-    const newLikeButton = imageElement.querySelector('.elements__like-button');
-    newLikeButton.addEventListener('click', function (evt) {
-        likeToggle(evt);
-    });
-    const picture = imageElement.querySelector('.elements__image');
-    picture.addEventListener('click', openImageViewPopup);
-    closeImageAddPopup();
+    closePopup(imagePopup);
 }
 function deleteCard(evt) {
     const eventTarget = evt.target;
@@ -159,13 +135,19 @@ function closeImageViewPopup() {
     viewPopup.classList.remove('popup_opened');
 }
 // Создание обработчиков пользовательских событий
+closeButtons.forEach((button) => {
+    const popup = button.closest('.popup');
+    button.addEventListener('click', () => closePopup(popup));
+  });
 //Для profile
-editProfileButton.addEventListener('click', openProfilePopup);
-ProfileCloseButton.addEventListener('click', closeProfilePopup);
-ProfileFormElement.addEventListener('submit', handleProfileFormSubmit);
+editProfileButton.addEventListener('click', function() {
+    openPopup(profilePopup);
+    startProfilePopup();
+});
+profileFormElement.addEventListener('submit', handleProfileFormSubmit);
 //Для image
-initializeArray(deleteButton, likeButton, imageContent);
-addButton.addEventListener('click', openImageAddPopup);
-ImageCloseButton.addEventListener('click', closeImageAddPopup);
-ImageFormElement.addEventListener('submit', handleImageFormSubmit);
-viewPopupCloseButton.addEventListener('click', closeImageViewPopup);
+addButton.addEventListener('click', function() {
+    openPopup(imagePopup);
+    imageForm.reset();
+});
+imageFormElement.addEventListener('submit', handleImageFormSubmit);
